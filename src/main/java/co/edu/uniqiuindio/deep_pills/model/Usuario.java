e419cdc69bda16930ff66abde7e3b888c34c8b8f
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-09-04 12:58:41 -0500

    Initial Commit

---
+++ src/main/java/co/edu/uniqiuindio/deep_pills/model/Usuario.java
@@ -0,0 +1,9 @@
+package co.edu.uniqiuindio.deep_pills.model;
+import java.io.Serializable;
+
+public abstract class Usuario implements Serializable {
+
+    private String username;
+    private String password;
+    private String correo;
+}
