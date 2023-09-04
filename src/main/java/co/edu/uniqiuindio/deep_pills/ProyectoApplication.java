b1dc195a7fdbc44fb46b92e5508d7867569e68ac
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-09-04 12:57:55 -0500

    Initial Commit

---
+++ src/main/java/co/edu/uniqiuindio/deep_pills/ProyectoApplication.java
@@ -0,0 +1,9 @@
+package co.edu.uniqiuindio.deep_pills;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ProyectoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoApplication.class, args);
+    }
+}
