a3afc1d90c80255c4bae04231ed8acfff46fe2df
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-10-16 00:06:03 -0500

    User Management Service Fully Tested

---
+++ src/test/java/deep_pills/UserManagementTest.java
@@ -0,0 +1,41 @@
+package deep_pills;
+
+import deep_pills.services.interfaces.UserManagementService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserManagementTest {
+    @Autowired UserManagementService userManagementService;
+
+    @Test public void enablePatientTest(){
+        try{
+            userManagementService.enablePatient(152L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test public void disablePatientTest(){
+        try{
+            userManagementService.disablePatient(152L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test public void enablePhysicianTest(){
+        try{
+            userManagementService.enablePhysician(52L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test public void disablePhysicianTest(){
+        try{
+            userManagementService.disablePhysician(52L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
